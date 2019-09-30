e2fcf3b5cc37b0a23d2e7c1ede088f70315477a9
kimiaki.sugiyama <sugiyamakimiaki@sugiyamakouryounoMacBook-puro.local>
2019-09-30 18:13:48 +0900

    nikki

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'diarys/index'
+  get 'diarys/show'
+  get 'diarys/new'
+  get 'diarys/create'
+  get 'diarys/edit'
+  get 'diarys/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
